5236b7601904aa3f33158806ace75b66d2dff90d
Jack <stanickj@bc.edum>
2024-12-20 16:35:39 -0500

    commit for making all of the files and the plan

--- MainFile.java
+++ MainFile.java
@@ -2,8 +2,7 @@ public class MainFile {
 
     public static void main (String [] args) { 
 
-        // This is going to be the main file for my checklist program. it has been a pain in the ass to set this all up
-        // i am adding this other comment to check again if everything is working
+        
 
     }
 
