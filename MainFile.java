0453e4dd3ce68e57285ba4e9fef882d99b8dff19
jackstanick04 <jackstanick04@gmail.com>
2024-12-20 16:15:35 -0500

    Add files via upload

---
+++ MainFile.java
@@ -0,0 +1,16 @@
+public class MainFile { 
+
+    public static int dog (int jack, String happy) {
+        return jack + 2;
+    }
+
+    public static void main (String [] args) { 
+
+        int num = 5;
+        System.out.print("Jack is gay");
+
+        System.out.println(dog(14, "Yes"));
+
+    }
+
+}
